ebedf554d2afceba491c484d75df878c810e36d8
Gerald Bauer <gerald.bauer@gmail.com>
2026-04-08 20:11:47 +0200

    up

--- sandbox/ppoutline.rb
+++ sandbox/ppoutline.rb
@@ -4,6 +4,13 @@ require_relative 'helper'
 ## pretty print (page) outline
 
 
+## let's you check optional ref e.g. ‹§fin›
+OPT_REF = %q{
+            (?: [ ]*    
+              ‹ (?<ref> §[^›]+?) ›
+            )?
+         }
+
 ##
 ##  note - do not match horizontal rule (hr)!!!
 ##     e.g.  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -16,81 +23,100 @@ HEADING_RE = %r{\A
            [ ]*
             (?<text>.+?)  ## use non-greedy; cannot be empty
    
-            (?: [ ]*    ## check optional ref e.g. ‹§fin›
-              ‹(?<ref> §[^›]+?)›
-            )?
-   
+            #{OPT_REF}
+          
              (?: [ ]*   ## optional trailing heading markers 
                  ={1,} 
              )?    
          [ ]* 
 \z}ix
 
-
+##########
 ## check "free standing" round headers e.g.
-##   Round 1
-##   Group A
+
+
+## allow optional winner or qualified team in bracket
+##   e.g. [West Germany]
+##     
+###  fix-fix-fix!!!         
+##  note - allow  [Brazil   -- better fix in 70q.txt!! 
+##
+## note - uses single-quote ('') string - backslash is backslash
+
+OPT_GROUP_NOTES = %q{
+                  (?: 
+                        [ ]*
+                     \[  [^\]]+  (?: \]|$)                     
+                  )?
+                 }
+
+
+##  -- special intercontinental groups
+GROUP_INTERCONTI_RE =%r{\A
+        [ ]*
+         (?<text>
+                     (?:   Europe 
+                         | South [ ] America 
+                         | Central [ ] and [ ] North [ ] America 
+                         | North [ ] and [ ] Central [ ] America [ ] and [ ] Paraguay 
+                         | North [ ] and [ ] Central [ ] America 
+                         | Asia [ ] and [ ] Africa 
+                         | Africa [ ] and [ ] Asia 
+                         | Asia [ ] and [ ] Oceania [ ]
+                         | Africa 
+                         | Asia 
+                         | [A-Z]+ (?: [ ][A-Z]+)*   ## fix - change to unicode alpha - why? why not? 
+                      ) 
+                     [ ]
+                    Group [ ] \d{1,2}
+                    #{OPT_GROUP_NOTES}
+         ) 
+       [ ]*
+\z}ix
+
+
+
+
+GROUP_RE = %r{\A
+        [ ]*
+         (?<text>
+           (?:    Group [ ]
+                 (?:   [A-Z]              ## e.g  group a, group b,
+                     | \d{1,2}            ##      group 1, group 2,
+                     | I | II | III | IV  ##      group i, group ii, etc.
+                  )
+                 #{OPT_GROUP_NOTES}   
+            )
+            | (?:  Subgroup [ ] (?: [A-Z] | \d{1,2} ))
+            |  Playoff:?         # used inside Group B etc. really Group A/B/C/D Playoff          
+         )                        
+        [ ]*
+\z}ix
 
 
 ROUND_RE = %r{\A
         [ ]*
          (?<text>
-             Round [ ] \d{1,2}  |   ## e.g. round 1, round 2, etc.
-              (?: First | Second | Third | 1st | 2nd | 3rd ) [ ] round |
+               Round [ ] \d{1,2}     ## e.g. round 1, round 2, etc.
+             | (?: First | Second | Third | 1st | 2nd | 3rd) [ ] round 
 
-             (?: First | Second | 1st | 2nd ) [ ] phase |
-               
+             | (?: First | Second | 1st | 2nd) [ ] phase 
+                         
+             | 1/8 [ ] Finals  
+             | Eightfinals 
 
-             ### todo/fix - move to GROUP_RE 
-             ##          why? why not?
-
-             (?:
-                (?:   ## allow optional qualifier - why? why not?
-                     (?: Europe |
-                         South [ ] America |
-                         Central [ ] and [ ] North [ ] America |
-                         North [ ] and [ ] Central [ ] America [ ] and [ ] Paraguay |
-                         North [ ] and [ ] Central [ ] America |
-                         Asia [ ] and [ ] Africa |
-                         Africa [ ] and [ ] Asia |
-                         Asia [ ] and [ ] Oceania [ ] Group |
-                         Africa |
-                         Asia 
-                      ) [ ]
-                )?
-                 (?:Sub)? Group [ ]
-                 (?:  [A-Z]   |         ## e.g  group a, group b,
-                      \d{1,2} |         ##      group 1, group 2,
-                     I | II | III | IV  ##      group i, group ii, etc.
-                  )
-                  (?:   ## allow optional winner or qualified team in brackets
-                        ## e.g. [West Germany]
-                     [ ]*
-                     \[[^]]+ (\]|$)   ## eg. [Brazil  -- better fix in 70q.txt!!
-                  )?
-             )  |  
-                                             
+             | Quarter [ -]? finals 
+             | Semi [ -]? finals 
              
-             
-             Playoff:?   |      # used inside Group B etc. really Group B Playoff
+             | Third [ ] place [ ] match 
+             | Match [ ] for [ ] third [ ] place 
 
-              1/8 [ ] Finals | 
-               Eightfinals |
-
-             Quarter [ -]? finals |
-             Semi [ -]? finals |
-             
-             Third [ ] place [ ] match |
-             Match [ ] for [ ] third [ ] place | 
-
-             Final |
-             Final group |  Final pool |
-             Deciding match
+             | Final 
+             | Final [ ] group | Final [ ] pool 
+             | Deciding [ ] match
          )
-        (?: [ ]*    ## check optional ref e.g. ‹§fin›
-            ‹(?<ref> §[^›]+?)›
-        )?
-        [ ]*
+             #{OPT_REF}
+         [ ]*
 \z}ix
 
 
@@ -111,10 +137,13 @@ def outline( txt )
              node << ref  if ref
             
              outline << node
-        elsif m=ROUND_RE.match( line )
-             text  = m[:text]
-             ref   = m[:ref]
-             node = [:"_", text, "@#{i+1}"]
+        elsif (m=ROUND_RE.match( line ) ||
+               m=GROUP_RE.match(line) ||
+               m=GROUP_INTERCONTI_RE.match(line))
+               captures = m.named_captures  
+               text  = captures['text']
+               ref   = captures['ref']
+              node = [:"_", text, "@#{i+1}"]
              node << ref  if ref
    
              outline << node       
@@ -169,10 +198,15 @@ def ppoutline( glob, workdir: )
 end
 
 
-buf = ppoutline( "./pages/**/*q.txt", workdir: '../worldcup')
+## buf = ppoutline( "./pages/**/*q.txt", workdir: '../worldcup')
+## buf = ppoutline( "./pages/**/*f.txt", workdir: '../worldcup')
+buf = ppoutline( "./pages/**/*full.txt", workdir: '../worldcup')
 
 puts buf
-write_text( "./tmp2/index-q.txt", buf )
+## write_text( "./tmp2/index-q.txt", buf )
+## write_text( "./tmp2/index-f.txt", buf )
+write_text( "./tmp2/index-full.txt", buf )
+
 
 puts "bye"
 
